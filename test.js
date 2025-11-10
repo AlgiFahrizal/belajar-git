4bb119817d5359ef5fb114d257c7aa5dafe1e2a9
Izulhak Algi Fahrizal <algifahrizal01012002@gmail.com>
2025-11-10 11:14:47 +0800

    Belajar Revert

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
+console.log ('Ini bagian tes');
 const add = (a, b) => a + b;
+<<<<<<< HEAD
 console.log (add(10, 20));
 
 const subtract = (a, b) => a - b;
 console.log (subtract(30, 10))
 
-const multiply = (a, b) => a * b;
-console.log (multiply(5, 6));
+const multiplyNumber = (a, b) => a * b;
+console.log (multiplyNumber(5, 6));
 
 const divide = (a, b) => a / b;
 console.log (divide(40, 8));
 
 const modulus = (a, b) => a % b;
 console.log (modulus(29, 5));
+=======
+console.log (add(10, 20));
+>>>>>>> parent of a00e15f (menghapus beberapa file)
---
+++ test2.js
@@ -0,0 +1,2 @@
+console.log('Ini bagian test 2');
+console.log('Ini bagian test 2 2');
