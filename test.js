824c462affea07a648e39e7e97bf5d097e7b72ca
Izulhak Algi Fahrizal <algifahrizal01012002@gmail.com>
2025-11-10 10:52:14 +0800

    Menambahkan fungsi devide dan modulus

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 const add = (a, b) => a + b;
 console.log (add(10, 20));
 
-const deduct = (a, b) => a - b;
+const subtract = (a, b) => a - b;
+console.log (subtract(30, 10))
+
+const multiply = (a, b) => a * b;
+console.log (multiply(5, 6));
+
+const divide = (a, b) => a / b;
+console.log (divide(40, 8));
+
+const modulus = (a, b) => a % b;
+console.log (modulus(29, 5));
